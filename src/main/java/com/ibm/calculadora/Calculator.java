3a95f482bc6b29b026943c7357832de701440a71
Joana Dias <dias.jo1992@gmail.com>
2022-11-14 11:44:47 -0300

    bug fix

---
+++ src/main/java/com/ibm/calculadora/Calculator.java
@@ -0,0 +1,25 @@
+package com.ibm.calculadora;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public double add(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public double sub(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public double division(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public double multiplication(double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+
+}
